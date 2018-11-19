72fd5c274182046e8c87a678f268098f9873125c
NXA <SimonNgj@users.noreply.github.com>
2018-11-19 22:00:03 +1100

    Update IMUarduino.h

--- IMUarduino.h
+++ IMUarduino.h
@@ -16,9 +16,9 @@ LSM9DS1 imu2;
 
 // Example I2C Setup //
 // SDO_XM and SDO_G are both pulled high, so our addresses are:
-#define LSM9DS1_M1 0x1E // Would be 0x1C if SDO_M is LOW
+#define LSM9DS1_M1   0x1E // Would be 0x1C if SDO_M is LOW
 #define LSM9DS1_AG1  0x6B // Would be 0x6A if SDO_AG is LOW
-#define LSM9DS1_M2 0x1C // Would be 0x1C if SDO_M is LOW
+#define LSM9DS1_M2   0x1C // Would be 0x1C if SDO_M is LOW
 #define LSM9DS1_AG2  0x6A // Would be 0x6A if SDO_AG is LOW
 int i = 0;
 
